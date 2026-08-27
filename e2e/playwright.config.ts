afff4ed1684286b776b49be88bf45790030ff5c7
Todo App Dev <dev@todo-app.local>
2026-08-27 08:29:48 +0300

    minor fix to playwright

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   fullyParallel: false,
   workers: 1,
   retries: process.env.CI ? 1 : 0,
-  reporter: 'list',
+  reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL,
     trace: 'on-first-retry',
